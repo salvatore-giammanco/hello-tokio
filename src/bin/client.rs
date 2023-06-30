cbc1ca61ed7ec884fd9e5fc95194d1548d22bed3
Salvatore Giammanco <salvatore@giammanco.me>
2023-06-30 18:02:19 +0200

    Add client

---
+++ src/bin/client.rs
@@ -0,0 +1,79 @@
+use bytes::Bytes;
+use tokio::sync::{mpsc, oneshot};
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Create a channel with a capacity of at most 32.
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
+
+    let manager = tokio::spawn(async move {
+        let mut client = mini_redis::client::connect("127.0.0.1:6379").await.unwrap();
+
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                },
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    let _ = resp.send(res);
+                },
+            }
+        }
+    });
+
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "foo".into(),
+            resp: resp_tx
+        };
+        if tx.send(cmd).await.is_err() {
+            eprintln!("Connection task shutdown");
+            return;
+        }
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".into(),
+            val: "bar".into(),
+            resp: resp_tx
+        };
+        if tx2.send(cmd).await.is_err() {
+            eprintln!("Connection task shutdown");
+            return;
+        }
+
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
+}
