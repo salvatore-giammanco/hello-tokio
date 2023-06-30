9c90b6f343c62882bde157f8799d059eb9cf9800
Salvatore Giammanco <salvatore@giammanco.me>
2023-06-30 22:39:12 +0200

    Add echo servers

---
+++ src/bin/echo-server.rs
@@ -0,0 +1,32 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return, // Remote has closed
+                    Ok(n) => {
+                        // Copy data back to the socket
+                        if socket.write_all(&buf[..n]).await.is_err() {
+                            // Unexpected socket error
+                            return;
+                        }
+                    },
+                    Err(_) => {
+                        // Unexpected socket error
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
